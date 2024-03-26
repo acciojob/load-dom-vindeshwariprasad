aad44ecbd7fb12ba3edad0111d9b1af4f3c28314
Vindeshwari Prasad <vinayguptasia53@gmail.com>
2024-03-27 04:21:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+    // Create a new paragraph element
+    var paragraph = document.createElement('p');
+    
+    // Set the text content of the paragraph
+    paragraph.textContent = 'DOM load success';
+    
+    // Append the paragraph to the body of the HTML document
+    document.body.appendChild(paragraph);
+});
+
