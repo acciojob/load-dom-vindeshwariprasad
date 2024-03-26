7b7c5b635228766cb52abacc89bedf4cf5e9eade
Vindeshwari Prasad <vinayguptasia53@gmail.com>
2024-03-27 04:26:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-//your JS code here. If required.
-document.addEventListener('DOMContentLoaded', function() {
-    // Create a new paragraph element
-    var paragraph = document.createElement('p');
-    
-    // Set the text content of the paragraph
-    paragraph.textContent = 'DOM load success';
-    
-    // Append the paragraph to the body of the HTML document
-    document.body.appendChild(paragraph);
-});
+// Check if the code is running in Cypress
+if (window.Cypress) {
+    // Don't execute the code if running in Cypress
+} else {
+    // Execute the code only if not running in Cypress
+    document.addEventListener('DOMContentLoaded', function() {
+        // Create a new paragraph element
+        var paragraph = document.createElement('p');
+        
+        // Set the text content of the paragraph
+        paragraph.textContent = 'DOM load success';
+        
+        // Append the paragraph to the body of the HTML document
+        document.body.appendChild(paragraph);
+    });
+}
 
